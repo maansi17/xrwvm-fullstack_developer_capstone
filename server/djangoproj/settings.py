76dc98e618e0a96ad7075aef0c74f3ab381b855d
maansi17 <mansichavan2121@gmail.com>
2024-10-01 09:57:01 -0400

    Added car make and model functionality, updated views and populated database

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', 'https://mansichavan2-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
 CSRF_TRUSTED_ORIGINS = [
     'https://mansichavan2-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/',
-    'https://mansichavan2-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+    'https://mansichavan2-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://mansichavan2-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
